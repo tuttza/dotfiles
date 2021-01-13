c80aefe383e21b6b10b9b0f643069489059b0597
Zach Tuttle <zachary.tuttle@crossoverhealth.com>
2021-01-13 13:52:47 -0800

    weekly updates. + adding sublime config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,8 @@
+# Ruby - rbenv:
+status --is-interactive; and source (rbenv init -|psub)
+
 # Functions:
+
 
 # GLOBAL ENV VARS:
 set -g -x KUBE_EDITOR kak
@@ -6,4 +10,6 @@ set -g -x SHELL /usr/local/bin/fish
 
 # Aliases:
 alias rbc="ruby -c"
-
+alias fs="grep -rni"
+alias gs="git status"
+alias ga="git add"
