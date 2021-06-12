9eb6628d6d3b2a1b93dc21d18ef96ad335050f50
Zach Tuttle <tuttle_zach@icloud.com>
2021-06-12 00:14:24 -0700

    Update sym_linker.pl

--- sym_linker.pl
+++ sym_linker.pl
@@ -6,18 +6,17 @@ use Cwd;
 use File::Basename;
 
 my $home = $ENV{"HOME"};
-my $current_dir = getcwd();
+my $current_dir = getcwd() . "/config";
 
 # The CONFIGS hash contains a list of key value pairs where
 # the key is full path for the config in your config repo,
 # and value is the location for which you want the symlink
 my %CONFIGS = (
-  "$current_dir/kakrc" => "$home/.config/kak/kakrc",
-  "$current_dir/zshrc" => "$home/.zshrc",
+  "$current_dir/kak/kakrc" => "$home/.config/kak/kakrc",
   "$current_dir/vimrc" => "$home/.vimrc",
-  "$current_dir/tmux.conf" => "$home/.tmux.conf",
-  "$current_dir/ssh_config" => "$home/.ssh/config",
-  "$current_dir/nanorc" => "$home/.nanorc"
+  #"$current_dir/tmux.conf" => "$home/.tmux.conf",
+  #"$current_dir/ssh_config" => "$home/.ssh/config",
+  #"$current_dir/nanorc" => "$home/.nanorc"
 );
 
 while( my($k, $v) = each %CONFIGS )
